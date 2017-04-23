8438aea3ec2f613a70ea3326ca690f15c1713e74
woshishuichen <kinn_jin@126.com>
2017-04-23 21:13:09 +0800

    checkOCI in php

---
+++ textoci.php
@@ -0,0 +1,18 @@
+<?php
+
+$conn = oci_connect('hr', 'hr_password', 'mymachine.mydomain/orcl');
+
+$stid = oci_parse($conn, 'select table_name from user_tables');
+oci_execute($stid);
+
+echo "<table>\n";
+while (($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
+    echo "<tr>\n";
+    foreach ($row as $item) {
+        echo "  <td>".($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;")."</td>\n";
+    }
+    echo "</tr>\n";
+}
+echo "</table>\n";
+
+?>
